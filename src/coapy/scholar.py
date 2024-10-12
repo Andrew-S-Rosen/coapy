0ad2e494ab4281c5991bb5c7fb84ec70729cfc85
Andrew Rosen <asrosen93@gmail.com>
2024-10-12 12:38:34 -0400

    Dump to csv by default

--- src/coapy/scholar.py
+++ src/coapy/scholar.py
@@ -1,6 +1,7 @@
 from __future__ import annotations
 
 import datetime
+from pathlib import Path
 from typing import TYPE_CHECKING
 
 from scholarly import scholarly
@@ -11,7 +12,9 @@ if TYPE_CHECKING:
 
 
 def get_coauthors(
-    scholar_id: str = "lHBjgLsAAAAJ", years_back: int | None = 2
+    scholar_id: str = "lHBjgLsAAAAJ",
+    years_back: int | None = 2,
+    filename: str | Path | None = "coauthors.csv",
 ) -> list[str]:
     """
     Given a Google Scholar ID, return a list of coauthors from the past N years.
@@ -22,8 +25,10 @@ def get_coauthors(
         Google Scholar ID of the author. This is the string of characters
         that appears in the URL of the author's Google Scholar profile
         immediately after "user=" and before "&hl=".
-    years_back : int
+    years_back : int | None
         Number of years to look back for coauthors. Set to `None` for no limit.
+    filename : str | Path | None
+        Path to the CSV file to write to, if any.
 
     Returns
     -------
@@ -35,9 +40,12 @@ def get_coauthors(
 
     profile = _get_scholar_profile(scholar_id)
 
-    return _get_coauthors_from_pubs(
+    co_authors = _get_coauthors_from_pubs(
         profile["publications"], year_cutoff=year_cutoff, my_name=profile["name"]
     )
+    if filename:
+        _dump_to_csv(co_authors, filename)
+    return co_authors
 
 
 def _get_scholar_profile(scholar_id: str, sections: list[str] | None = None) -> Author:
@@ -50,7 +58,7 @@ def _get_scholar_profile(scholar_id: str, sections: list[str] | None = None) -> 
         Google Scholar ID of the author. This is the string of characters
         that appears in the URL of the author's Google Scholar profile
         immediately after "citations?user=" and before "&hl=".
-    sections : list[str]
+    sections : list[str] | None
         Sections of the profile to return. If None, return the default
         sections selected by scholarly.
 
@@ -77,10 +85,10 @@ def _get_coauthors_from_pubs(
     ----------
     papers : list[Publication]
         List of publications.
-    year_cutoff : int
+    year_cutoff : int | None
         Year before which to ignore publications. If set to `None`, all
         publications will be considered.
-    my_name : str
+    my_name : str | None
         Name of the author. If set to `None`, the author will still be
         included in the list of co-authors.
 
@@ -132,3 +140,24 @@ def _nsf_name_cleanup(coauthors: list[str]) -> list[str]:
         reordered_name = f"{name_parts[-1]}, {' '.join(name_parts[:-1])}"
         cleaned_coauthors.append(reordered_name)
     return cleaned_coauthors
+
+
+def _dump_to_csv(co_authors: list[str], filename: str | Path = "coauthors.csv") -> None:
+    """
+    Dump a list of coauthors to a CSV file.
+
+    Parameters
+    ----------
+    co_authors : list[str]
+        List of coauthors.
+    filename : str | Path
+        Name of the CSV file to write to.
+
+    Returns
+    -------
+    None
+    """
+
+    with Path(filename).open(mode="w", encoding="utf-8") as f:
+        for coauthor in co_authors:
+            f.write(f"{coauthor}\n")
--- tests/test_scholar.py
+++ tests/test_scholar.py
@@ -1,4 +1,8 @@
+from pathlib import Path
+
 from coapy.scholar import get_coauthors
 
-def test_scholar():
+def test_scholar(monkeypatch, tmp_path):
+  monkeypatch.chdir(tmp_path)
   assert get_coauthors()
+  assert Path("coauthors.csv").exists()
